f40e9447d9ca8f6e7a8788cc2e14842fd1ff4f14
mthayz <michellethayanne@outlook.com>
2025-03-10 12:43:36 -0300

    Corrigido conflito em src/server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ server.use('/public', publicRoutes) //rotas na porta 8000
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
-server.use(router) //rotas na prota 3000
+server.use(router) //rotas na porta 3000
 
 server.listen(8000, () => {
   console.log("Boas vindas a API do AllBooks")
